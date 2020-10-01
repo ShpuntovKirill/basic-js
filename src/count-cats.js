b8b987641ca808e5665728db06292e0010ca3091
Shpuntov Kirill <ks090192@gmail.com>
2020-10-02 02:35:25 +0300

    cut count-cats code

--- src/count-cats.js
+++ src/count-cats.js
@@ -5,13 +5,10 @@ module.exports = function countCats(matrix) {
     var arr = [];
     for (i = 0; i < matrix.length; i++) {
       for (j = 0; j < matrix[i].length; j++) {
-        arr.push(matrix[i][j]);
-      }
-    }
-    for (var i=0; i < arr.length; i++) {
-      if (arr[i] == '^^') {
-        count++
-      }
-  }
+        if (matrix[i][j] == '^^') {
+          count++
+        };
+      };
+    };
   return count
 };
