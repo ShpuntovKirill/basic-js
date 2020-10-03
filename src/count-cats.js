241a0de643ed9799a325a1fec20deef6c2ddc0d4
Shpuntov Kirill <ks090192@gmail.com>
2020-10-03 23:17:58 +0300

    feat: done crbon-dating task

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
     var count = 0;
-    var arr = [];
     for (i = 0; i < matrix.length; i++) {
       for (j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j] == '^^') {
